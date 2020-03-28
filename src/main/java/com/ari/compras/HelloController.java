3f0cd2ec58091ad53536e97f1ba39b9a044fd3db
Aristoteles <aristotelesbr2014@gmail.com>
2020-03-28 19:49:46 -0300

    first commit

---
+++ src/main/java/com/ari/compras/HelloController.java
@@ -0,0 +1,18 @@
+package com.ari.compras;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+/**
+ * HelloController
+ */
+@RestController
+@RequestMapping("/hello")
+public class HelloController {
+
+  @GetMapping
+  public String hello() {
+    return "Hello!";
+  }
+}
